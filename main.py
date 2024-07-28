f00570aeb6429961e2dd3ae38b2c21d8efcd8d9d
weilai0415@126.com <weilai0415@126.com>
2024-07-28 19:30:49 +0800

    upload some images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding: utf8
-
 from urllib.request import urlopen 
-from bs4 import BeautifulSoup 
-html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html") 
+from bs4 import BeautifulSoup
+
+
+html = urlopen("https://www.simplyquotes.com/quotes")
 bsObj = BeautifulSoup(html)
