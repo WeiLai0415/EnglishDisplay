a65333f77b2b85873164d3273ba1d22d33472c2e
weilai0415@126.com <weilai0415@126.com>
2024-07-28 19:01:57 +0800

    initialization

---
+++ main.py
@@ -0,0 +1,6 @@
+# coding: utf8
+
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup 
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html") 
+bsObj = BeautifulSoup(html)
